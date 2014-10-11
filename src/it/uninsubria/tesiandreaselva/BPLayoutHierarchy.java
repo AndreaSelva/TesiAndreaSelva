2bd45bb7219d00ab8348e74964aee83032cf0d39
AndreaSelva <andrea.selva90@gmail.com>
2014-10-11 11:07:25 +0200

    programmally layout
    la composizione del layout è stato realizzato completamente da codice.

--- src/it/uninsubria/tesiandreaselva/BPLayoutHierarchy.java
+++ src/it/uninsubria/tesiandreaselva/BPLayoutHierarchy.java
@@ -4,14 +4,51 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class BPLayoutHierarchy extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_bplayout_hierarchy);
-		ViewServer.get(this).addWindow(this);
+		
+		RelativeLayout rootLayout = new RelativeLayout(this); //creo relative
+		RelativeLayout.LayoutParams relLayoutParam = new RelativeLayout.LayoutParams
+				(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT); // params root
+		rootLayout.setPadding(25, 25, 0, 0);
+		
+		
+		RelativeLayout.LayoutParams lpViewTop = new RelativeLayout.LayoutParams
+				(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT); //parametri textview top
+		lpViewTop.addRule(RelativeLayout.ALIGN_PARENT_TOP);
+		TextView tv = new TextView(this);
+		tv.setId(1);//set id a 1 poichè a 0 non funziona 
+        tv.setText("Best Practices Layout Hierarchy");
+        
+        rootLayout.addView(tv, lpViewTop); //aggiungo la textview
+        
+        RelativeLayout.LayoutParams lpViewBelow = new RelativeLayout.LayoutParams //creo parametri per quelli sotto 
+				(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        lpViewBelow.addRule(RelativeLayout.BELOW, tv.getId());
+        
+        for(int i=2; i<45; i++){ //nell'ordine di secondi riesce ad agire anche con 10000 textview
+        	
+        	tv = new TextView(this); 	//creo ricorsivamente le text viev aggiungendole
+        	tv.setId(i);
+            tv.setText("Best Practices Layout Hierarchy");
+        	rootLayout.addView(tv, lpViewBelow);
+        	
+        	lpViewBelow = new RelativeLayout.LayoutParams  //devo ricreare i parametri per poter aggiornare le rule
+    				(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        	lpViewBelow.addRule(RelativeLayout.BELOW, tv.getId());
+        	
+        }
+        
+        setContentView(rootLayout, relLayoutParam); //creo l intera vista
+        //setContentView(R.layout.activity_bplayout_hierarchy);
+		
 	}
 	
 	@Override
--- src/it/uninsubria/tesiandreaselva/NBPLayoutHierarchy.java
+++ src/it/uninsubria/tesiandreaselva/NBPLayoutHierarchy.java
@@ -4,13 +4,48 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class NBPLayoutHierarchy extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_nbplayout_hierarchy); 
+		
+		//setContentView(R.layout.activity_nbplayout_hierarchy); 
+		
+		LinearLayout rootLayout = new LinearLayout(this); //layout root
+		rootLayout.setOrientation(LinearLayout.VERTICAL);
+		LayoutParams linLayoutParam = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);  // params root
+		rootLayout.setPadding(25, 25, 0, 0);
+		
+		LayoutParams lpView = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT); //parametri textview
+        TextView tv = new TextView(this); //creo prima textview
+        tv.setText("Not Best Practices Layout Hierarchy");
+        
+        rootLayout.addView(tv, lpView); //inserisco la textview nella rootLinearLayout
+        
+        LinearLayout fatherLayout = rootLayout; //copio il root per usarlo in seguito nell'algoritmo
+        
+		for(int i=0; i<43; i++){ // MAX num di elementi 44 textview (stackoverflowerror)
+			
+			LinearLayout childLayout = new LinearLayout(this); //creo childLayout
+			childLayout.setOrientation(LinearLayout.VERTICAL);
+			fatherLayout.addView(childLayout, linLayoutParam); //aggiungo childLayout al fatherLayout
+			
+			tv = new TextView(this); //creo nuovo text view
+		    tv.setText("Not Best Practices Layout Hierarchy");
+			childLayout.addView(tv, lpView);  //inserisco nuovo textview al childLayout
+			
+			fatherLayout=childLayout; //ora il child deve diventare father e si cicla
+			
+        }
+		
+		setContentView(rootLayout, linLayoutParam);
+		//setContentView(R.layout.activity_nbplayout_hierarchy);
+		
 		ViewServer.get(this).addWindow(this);
 	}
 	
